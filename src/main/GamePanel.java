fe0491dae062df7505e9a9fe9238e641b46da89e
joelrennert <jrennert@gmail.com>
2023-08-02 13:40:58 -0400

    Added title screen and some things

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,10 +5,12 @@ import inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
+
+    private final int SCREEN_WIDTH = 600;
+    private final int SCREEN_HEIGHT = 600;
+
     private MouseInputs mouseInputs;
     public int xDelta = 100, yDelta = 100;
 
@@ -19,22 +21,45 @@ public class GamePanel extends JPanel {
         addMouseMotionListener(mouseInputs);
         setFocusable(true);
 
+        // Background color
+        this.setBackground(Color.darkGray);
     }
+
     public void changeXDelta(int value){
         this.xDelta += value;
         repaint();
     }
+
     public void changeYDelta(int value){
         this.yDelta += value;
         repaint();
     }
+
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
         repaint();
     }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta, 200, 50);
+        // draw title on background, might need to do it differently
+        drawTitle(g);
+
+        // mouse follower thing
+        g.setColor(Color.gray);
+        g.fillRoundRect(xDelta, yDelta, 20, 20, 5, 5);
+
+    }
+
+    public void drawTitle(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("FUTURA", Font.ITALIC, 50));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.setColor(Color.RED);
+        g.drawString("SONS OF ARES", (SCREEN_WIDTH - metrics2.stringWidth("SONS OF ARES")) / 2, SCREEN_HEIGHT / 2);
+        g.setColor(Color.GRAY);
+        g.drawString("SONS OF ARES", (SCREEN_WIDTH - metrics2.stringWidth("SONS OF ARES")) / 2 -1, SCREEN_HEIGHT / 2 -1);
+
     }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -7,10 +7,11 @@ import javax.swing.*;
 public class GameWindow extends JFrame {
     public GameWindow(GamePanel gamePanel){
         JFrame jFrame = new JFrame();
-        jFrame.setSize(400, 400);
+        jFrame.setSize(600, 600);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
         jFrame.setVisible(true);
         
     }
