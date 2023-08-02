c9b9813faa2500215ca55523ee17ec0fbfdb6c53
Jason Scott <856lowcard@gmail.com>
2023-08-02 11:08:39 -0400

    added keyboard inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,11 +1,15 @@
 package main;
 
+import inputs.KeyboardInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
     public GamePanel(){
-
+        addKeyListener(new KeyboardInputs());
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -8,6 +8,7 @@ public class GameWindow extends JFrame {
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
 }
